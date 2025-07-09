c3165c3a586ab54b8ee908bb5cef4dbf1c77df59
Duy An Tran <58235835+buccancs@users.noreply.github.com>
2025-07-09 17:04:41 +0100

    update

--- platforms/android/app/build.gradle.kts
+++ platforms/android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.buccancs.gsrcapture"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.buccancs.gsrcapture"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
